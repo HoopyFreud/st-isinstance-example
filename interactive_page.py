8a7de18daff9d0109bb83acfa912298cba96648e
HoopyFreud <HoopyFreud@users.noreply.github.com>
2024-10-09 15:09:52 -0400

    Update interactive_page.py
    Added a button to re-run the page with no other changes

--- interactive_page.py
+++ interactive_page.py
@@ -4,7 +4,9 @@ import streamlit as st
 def initObj():
     st.session_state.testObj = cd.testclass() 
     
-st.button("Initialize", on_click = initObj) 
+st.button("Initialize", on_click=initObj) 
+    
+st.button("Rerun") 
 
 if "testObj" in st.session_state:
     st.write("isinstance working? - " + str(isinstance(st.session_state.testObj, cd.testclass)))
