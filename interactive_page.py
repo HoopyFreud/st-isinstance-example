4d4064473dc03f74fc3dea380020982688580d65
HoopyFreud <HoopyFreud@users.noreply.github.com>
2024-10-09 14:54:31 -0400

    Add files via upload

---
+++ interactive_page.py
@@ -0,0 +1,12 @@
+import classDef as cd
+import streamlit as st
+
+def initObj():
+    st.session_state.testObj = cd.testclass() 
+    
+st.button("Initialize", on_click = initObj) 
+
+if "testObj" in st.session_state:
+    st.write("isinstance working? - " + str(isinstance(st.session_state.testObj, cd.testclass)))
+else:
+    st.write("Not initialized")
